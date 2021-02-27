ee2087700b494d0430c9844ff4b82bfa5efe1fc1
Adithya Narayan Rangarajan Sreenivasan <e0426343@u.nus.edu>
2021-02-27 22:08:49 +0800

    setup endpoints

--- backend/server.py
+++ backend/server.py
@@ -2,10 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stock = {}
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/stocks', methods=['GET'])
+def get_stocks():
+    return stock
+
+@app.route('/stocks', methods=['POST'])
+def update_stock():
+    return "updated"
+
+@app.route('/stock/<stock_id>', methods=['GET'])
+def get_stock(stock_id):
+    if stock_id in stock.keys():
+        return stock[stock_id]
+    return stock_id + " not found"
 
 if __name__ == "__main__":
     app.run(debug=True)
