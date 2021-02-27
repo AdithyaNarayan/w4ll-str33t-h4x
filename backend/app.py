432ccc1e146e2fbe3ff891dc30a4b3dfb840d8c4
chrischongyj <chrischongyj@gmail.com>
2021-02-28 03:09:03 +0800

    Added frontend

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import jsonify
 import praw
 from praw.models import MoreComments
+from os.path import join, dirname, realpath
 
 app = Flask(__name__)
 reddit = praw.Reddit(client_id='tLNJPqcv90IWWQ', client_secret='f8viKi1NaK7CAjLX8wv24CAl-DggnQ', user_agent='w4ll-str33t-h4x')
 
 stock = {}
 
-@app.route('/stocks/<num>', methods=['GET'])
-def get_stocks(num):
-    try:
-        no_of_stocks = int(num)
-        stocks = sorted(stock.items(), key=lambda keyvaluepair: -keyvaluepair[1])
-        return jsonify(stocks[:no_of_stocks])
-    except:
-        return "error"
-
-@app.route('/stocks', methods=['POST'])
+@app.route('/', methods=['GET'])
 def update_stock():
     posts = reddit.subreddit('wallstreetbets').hot(limit=1000)
     search_str = ""
     for post in posts:
         search_str += post.title
     
-    f = open("stocks/stocks.txt", "r")
+    f = open(join(dirname(realpath(__file__)),"..", "stocks\\stocks.txt"), "r")
     for line in f.readlines():
         code = line.split('-')[0].strip()
         if len(code) < 3:
@@ -32,8 +24,16 @@ def update_stock():
         num = search_str.count(code)
         if num > 0:
             stock[code] = num
+
+    stocks = sorted(stock.items(), key=lambda keyvaluepair: -keyvaluepair[1])
     
-    return "updated"
+    return render_template('index.html', stocks=stocks[:10])
+
+
+@app.route('/stonks', methods=['GET'])
+def update_stonk():
+    stocks = [('AMC', 15), ('GME', 10),('AMC', 15), ('GME', 10),('AMC', 15), ('GME', 10),('AMC', 15), ('GME', 10),('AMC', 15), ('GME', 10)]
+    return render_template('index.html', stocks=stocks[:10])
 
 if __name__ == "__main__":
     app.run(debug=True)
